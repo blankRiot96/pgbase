f94cba703df9a8ba42ea508a21e577f95c283242
blankRiot96 <blankRiot96@gmail.com>
2024-09-28 16:13:19 +0530

    initial commit

---
+++ base_setup.py
@@ -0,0 +1,14 @@
+import os
+
+os.remove(".git")
+os.system("pre-commit install")
+
+os.system("git init")
+os.system("git add .gitignore")
+os.system("git commit -m 'added .gitignore'")
+os.system("git add .")
+os.system("git commit -m 'initial commit'")
+
+os.system("py -m venv venv")
+os.system("source venv/bin/activate.fish")
+os.system("pip install -r requirements.txt")
